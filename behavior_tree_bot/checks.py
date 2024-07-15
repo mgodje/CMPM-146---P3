eed7d60d295794c0ded53e674062a0f46a4c57e8
mgodje <114205884+mgodje@users.noreply.github.com>
2024-07-15 15:52:01 -0700

    starting checks

--- behavior_tree_bot/checks.py
+++ behavior_tree_bot/checks.py
@@ -9,3 +9,14 @@ def have_largest_fleet(state):
              + sum(fleet.num_ships for fleet in state.my_fleets()) \
            > sum(planet.num_ships for planet in state.enemy_planets()) \
              + sum(fleet.num_ships for fleet in state.enemy_fleets())
+
+def is_under_threat(state):
+    # Implement a check to see if any planets are under immediate threat
+    
+    pass
+def if_beneficial_neutral_planet_available(state):
+    # Implement a check for beneficial neutral planets
+    pass
+def have_strategic_advantage(state):
+    # Implement a check to determine if attacking now is advantageous
+    pass
